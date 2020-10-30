d6857bb9323cacaa0e2e0e1db09c2808247033a6
Opal13049 <71312988+Opal13049@users.noreply.github.com>
2020-10-30 18:01:08 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello world</title>
+    </head>
+    <body>
+        <h1>Introduce myself</h1>
+        <p>My name is Korrawit Prasertdee</p>
+        <p>My nickname is Opal</p>
+        <h2>แนะนำตัว</h2>
+        <p>สวัสดีครับผมชื่อ นายกรวิชญ์ ประเสริฐดี ชื่อเล่นชื่อ โอปอล</p>
+        <img src="a.png">
+        <img src="b.png">
+        <br>
+        <h2><a href="http://youtube.com">This is my favorite Link</a></h2>
+
+        <h1>My joke</h1>
+        <h2>เมื่อวานครับครับ</h2>
+        <p>นาย A: เมื่อวานนี้…พาแฟนไปเที่ยวเสม็ดมา
+            นาย B: เหรอๆ ไปเสม็ด เสร็จทุกรายป่ะวะ
+            นาย A: พอตกกลางคืนนี่…ไม่ได้นอนเลย
+            นาย B: โอ้โห !!! ซั่ม กันจนถึงเช้า?
+            นาย A: ไปเช้า-เย็นกลับ
+            นาย B: ……</p>
+    
+    </body>
+</html>
